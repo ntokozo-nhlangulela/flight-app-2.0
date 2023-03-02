c59b3b527fcce783d2f9acb70ffb82d2387e5e01
ntokozo-nhlangulela <nnhlangulela@jhb.dvt.co.za>
2023-03-02 08:41:46 +0200

    Initial observable commit

--- src/plane-track.ts
+++ src/plane-track.ts
@@ -2,7 +2,7 @@ import * as L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 import '../dist/output.css';
-import { flightApiCall } from "./api/api";
+import { flightApiCall$ } from "./api/api";
 
 
 let listContainer = document.querySelector("#flight-list");
@@ -13,12 +13,59 @@ function flightHandler(coordinates: { lat: any; long: any; }) {
     //setFlightLocation(coordinates.lat, coordinates.long);
     map1.flyTo([coordinates.long, coordinates.lat], 11);
 }
+// let flights = [];
 
-const flights = await flightApiCall();
+// flights = flightApiCall$;
+// localStorage.setItem('flightData', JSON.stringify(flights));
+// let test = localStorage.getItem('flightData');
+// console.log(test);
+// console.log('These are the flights:', flights);
+
+const flights = await flightApiCall$();
 localStorage.setItem('flightData', JSON.stringify(flights));
-let test = localStorage.getItem('flightData');
-console.log(test);
-console.log('These are the flights:', flights);
+console.log(flights);
+
+if (localStorage !== null)
+    for (let i = 0; i < 20; i++) {
+        let coordinates = {
+            lat: 0,
+            long: 0,
+        };
+        console.log('In ho')
+        const flightTable = document.createElement("tr");
+        const flightTableData = document.createElement("td");
+        flightTableData.innerText = flights[i][1];
+        const flightTableData2 = document.createElement("td");
+        flightTableData2.innerText = flights[i][2];
+        const flightTableData3 = document.createElement("td");
+        flightTableData3.innerText = flights[i][9];
+
+        flightTable.appendChild(flightTableData);
+        flightTable.appendChild(flightTableData2);
+        flightTable.appendChild(flightTableData3);
+        flightTable.className = "flex-item";
+
+        listContainer?.appendChild(flightTable);
+        coordinates.lat = flights[i][5];
+        coordinates.long = flights[i][6];
+        flightTable.addEventListener("click", () => flightHandler(coordinates));
+    }
+else {
+    console.log('No previous history')
+}
+// const flights=[];
+// flightApiCall$.subscribe((flightResults) => {
+//     const flights = flightResults;
+//     // localStorage.setItem('flightData', JSON.stringify(flights));
+//     // let test = localStorage.getItem('flightData');
+//     // console.log(test);
+//     localStorage.setItem('Test', 'This is a test.')
+//     debugger;
+//     (flights);
+
+// });
+
+
 
 for (let i = 0; i < 20; i++) {
     let coordinates = {
