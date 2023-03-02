48cf7447f293b221ec1aa61b8048d29460abc61c
ntokozo-nhlangulela <nnhlangulela@jhb.dvt.co.za>
2023-03-02 03:32:59 +0200

    feat: :ambulance: Made an API call
    Made an API call to openskynetwork to fetch list of 20 flights.

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,20 @@
-import { fromFetch } from "rxjs/fetch"
+export async function flightApiCall() {
+    const flightApi = "https://opensky-network.org/api/states/all";
+    const flights = await fetch(flightApi)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            return data.states.slice(0, 20);
+            //console.log(apiResult);
+        });
 
-export const fetchFlights$ = fromFetch('https://opensky-network.org/api/states/all', {
-    selector: (response) => response.json()
-})
+    return flights;
+}
 
 
+
+//import { fromFetch } from "rxjs/fetch"
+
+// export const fetchFlights$ = fromFetch('https://opensky-network.org/api/states/all', {
+//     selector: (response) => response.json()
+// })
