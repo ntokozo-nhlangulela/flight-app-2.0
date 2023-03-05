9619f91349f1d20442661512c58dfc2b8dc26c51
ntokozo-nhlangulela <nnhlangulela@jhb.dvt.co.za>
2023-03-06 01:53:42 +0200

    feat(Tailwind): :art: Using tailwind instead of css.
    Changed css code to use tailwind.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        8: "2rem",
+        12: "3rem",
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'purple': '#3f3cbb',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+      },
+    },
   },
   plugins: [],
 };
